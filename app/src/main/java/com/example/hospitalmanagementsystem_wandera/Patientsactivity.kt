b4b8e14cba043b9a8f4eefd9b311d5c046859865
THEEST005 <wanderastecy64@gmail.com>
2023-05-08 11:53:58 +0300

    hospital management

---
+++ app/src/main/java/com/example/hospitalmanagementsystem_wandera/Patientsactivity.kt
@@ -0,0 +1,11 @@
+package com.example.hospitalmanagementsystem_wandera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Patientsactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patientsactivity)
+    }
+}
